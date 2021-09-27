d0f1ce1c6a41f4f1715a8eb4e1eefd8aaa918c28
Nojus R @home <nojusr@gmail.com>
2021-09-27 20:47:48 +0300

    implemented the whole api

--- internal/kelp/api.go
+++ internal/kelp/api.go
@@ -34,7 +34,7 @@ func FetchUserStats(w http.ResponseWriter, r *http.Request) {
 func FetchUserUploads(w http.ResponseWriter, r *http.Request) {
 	user := KelpUser{}
 
-	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user)
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
 
 	if err != nil {
 		respondError(w, 404, "user not found")
@@ -44,7 +44,7 @@ func FetchUserUploads(w http.ResponseWriter, r *http.Request) {
 	//Db.Find(&projects)
 	files := []KelpFile{}
 
-	err = Db.Where(&KelpFile{UserId: int(user.ID)}).Find(&files)
+	err = Db.Where(&KelpFile{UserId: int(user.ID)}).Find(&files).Error
 
 	if err != nil {
 		respondError(w, 500, "failed to retreive files")
@@ -115,11 +115,11 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	filename := generateRandomString(6)
+	fileName := generateRandomString(6)
 	fileType := trimFirstRune(filepath.Ext(handler.Filename)) // trimmed in order to preserve db structure
 	orgName := strings.Split(handler.Filename, ".")[0]
 
-	err = ioutil.WriteFile(fmt.Sprintf("./static/u/%s.%s", filename, fileType), fileBytes, 0444)
+	err = ioutil.WriteFile(fmt.Sprintf("./static/u/%s.%s", fileName, fileType), fileBytes, 0444)
 
 	if err != nil {
 		respondError(w, 500, "failed to write file")
@@ -129,7 +129,7 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	newFile := KelpFile{
 		UserId:  int(user.ID),
 		Type:    fileType,
-		Name:    filename,
+		Name:    fileName,
 		OrgName: orgName,
 	}
 
@@ -137,36 +137,159 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		respondError(w, 500, "failed to add db entry")
-		os.Remove(fmt.Sprintf("./static/u/%s.%s", filename, fileType))
+		os.Remove(fmt.Sprintf("./static/u/%s.%s", fileName, fileType))
 		return
 	}
 
 	defer file.Close()
+
+	respondJSON(w, 200, map[string]string{
+		"filesize": byteCountSI(int64(len(fileBytes))),
+		"file_id":  fileName,
+		"filename": fmt.Sprintf("%s.%s", fileName, fileType),
+		"link":     fmt.Sprintf("%s/u/%s.%s", r.Host, fileName, fileType),
+	})
 }
 
 // delete a file
 func DeleteFile(w http.ResponseWriter, r *http.Request) {
+	user := KelpUser{}
+	fileToDelete := KelpFile{}
+
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
+
+	if err != nil {
+		respondError(w, 404, "user not found")
+		return
+	}
+
+	err = Db.Where(&KelpFile{Name: r.PostFormValue("file_id")}).First(&fileToDelete).Error
+
+	if err != nil {
+		respondError(w, 404, "file not found")
+		return
+	}
+
+	deleteFileIfExists(fmt.Sprintf("./static/u/%s.%s", fileToDelete.Name, fileToDelete.Type))
+
+	respondJSON(w, 200, map[string]string{
+		"success": "true",
+	})
 
 }
 
 // delete all files
 func DeleteAllFiles(w http.ResponseWriter, r *http.Request) {
+	user := KelpUser{}
+	filesToDelete := []KelpFile{}
+
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
+
+	if err != nil {
+		respondError(w, 404, "user not found")
+		return
+	}
+
+	err = Db.Where(&KelpFile{UserId: int(user.ID)}).Find(&filesToDelete).Error
+
+	if err != nil {
+		respondError(w, 500, "failed to retreive files for deletion")
+		return
+	}
+
+	for _, file := range filesToDelete {
+		deleteFileIfExists(fmt.Sprintf("./static/u/%s.%s", file.Name, file.Type))
+	}
+
+	Db.Where(&KelpFile{UserId: int(user.ID)}).Delete(KelpFile{})
+
+	respondJSON(w, 200, map[string]string{
+		"success": "true",
+	})
 
 }
 
 // upload a paste
 func UploadPaste(w http.ResponseWriter, r *http.Request) {
+	user := KelpUser{}
+
+	pasteID := generateRandomString(6)
+
+	newPaste := KelpPaste{
+		Name:    pasteID,
+		Title:   r.PostFormValue("paste_name"),
+		Content: r.PostFormValue("u_paste"),
+	}
+
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
+
+	if err != nil {
+		respondError(w, 404, "user not found")
+		return
+	}
+
+	err = Db.Create(newPaste).Error
+
+	if err != nil {
+		respondError(w, 500, "failed to create paste")
+		return
+	}
+
+	respondJSON(w, 200, map[string]string{
+		"paste_id": pasteID,
+		"link":     fmt.Sprintf("%s/p/%s", r.Host, pasteID),
+		"raw_link": fmt.Sprintf("%s/p/raw/%s", r.Host, pasteID),
+	})
 
 }
 
 // delete a paste
 func DeletePaste(w http.ResponseWriter, r *http.Request) {
+	user := KelpUser{}
+	pasteToDelete := KelpPaste{}
 
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
+
+	if err != nil {
+		respondError(w, 404, "user not found")
+		return
+	}
+
+	err = Db.Where(&KelpPaste{Name: r.PostFormValue("paste_id")}).First(&pasteToDelete).Error
+
+	if err != nil {
+		respondError(w, 500, "paste not found")
+		return
+	}
+
+	Db.Delete(&pasteToDelete)
+
+	respondJSON(w, 200, map[string]string{
+		"success": "true",
+	})
 }
 
 // delete all pastes
 func DeleteAllPastes(w http.ResponseWriter, r *http.Request) {
+	user := KelpUser{}
 
+	err := Db.Where(&KelpUser{ApiKey: r.PostFormValue("api_key")}).First(&user).Error
+
+	if err != nil {
+		respondError(w, 404, "user not found")
+		return
+	}
+
+	err = Db.Where(&KelpPaste{UserId: int(user.ID)}).Delete(KelpPaste{}).Error
+
+	if err != nil {
+		respondError(w, 500, "failed to delete pastes")
+		return
+	}
+
+	respondJSON(w, 200, map[string]string{
+		"success": "true",
+	})
 }
 
 // common call to return JSON
@@ -220,4 +343,26 @@ func generateRandomString(n int) string {
 func trimFirstRune(s string) string {
 	_, i := utf8.DecodeRuneInString(s)
 	return s[i:]
+}
+
+// https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+func byteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func deleteFileIfExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil
+	}
+	return os.Remove(path)
 }
